b3cd27643b21e534c3a0e5c64b6154f06767b348
HZie <boss.hz734@gmail.com>
2021-12-02 10:55:16 +0900

    js: update prices in sum

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const input_name = document.querySelector('.input_name');
 const input_price = document.querySelector('.input_price');
 const add_button = document.querySelector('.add_button');
 const itemList = document.querySelector('.item-list');
+let checkedPrice = 0;
+let uncheckedPrice = 0;
 
 add_button.addEventListener('click', () => {
   const itemName = input_name.value;
-  let itemPrice = input_price.value;
+  let itemPrice = parseInt(input_price.value);
   if (itemName == '') {
     alert('Please enter the item name.');
     return;
@@ -19,14 +21,18 @@ add_button.addEventListener('click', () => {
 
   const item = document.createElement('li');
   item.setAttribute('class', 'item');
+  item.setAttribute('data-price', `${itemPrice}`);
   item.innerHTML = `
           <div class="item_name">${itemName}</div>
-          <div class="item_price">${itemPrice}</div>
+          <div class="item_price">${itemPrice} Won</div>
           <i class="item_delete-button far fa-trash-alt"></i>
   `;
   itemList.appendChild(item);
   input_name.value = '';
   input_price.value = '';
+
+  uncheckedPrice += itemPrice;
+  updateSummary();
 });
 
 // handle delete checked item button
@@ -36,21 +42,44 @@ deleteCheckedBtn.addEventListener('click', () => {
   items.forEach((item) => {
     if (item.classList.contains('checked')) item.remove();
   });
+  uncheckedPrice = 0;
+  updateSummary();
 });
 
 // handle toggling item when item is clicked and delete item when delete button is clicked
 itemList.addEventListener('click', (event) => {
+  const currPrice = parseInt(event.target.parentNode.dataset.price);
   if (event.target.classList.contains('item_delete-button')) {
     event.target.parentNode.remove();
+    if (event.target.parentNode.classList.contains('checked')) {
+      checkedPrice -= currPrice;
+    } else {
+      uncheckedPrice -= currPrice;
+    }
   } else {
-    event.target.parentNode.classList.toggle('checked');
+    if (event.target.parentNode.classList.contains('checked')) {
+      event.target.parentNode.classList.remove('checked');
+      uncheckedPrice += currPrice;
+      checkedPrice -= currPrice;
+    } else {
+      event.target.parentNode.classList.add('checked');
+      checkedPrice += currPrice;
+      uncheckedPrice -= currPrice;
+    }
   }
+  updateSummary();
 });
 
-// TO DO
+// update sum when item is added / deleted / checked / unchecked
+const sumChecked = document.querySelector('.sum_checked-price');
+const sumUnchecked = document.querySelector('.sum_unchecked-price');
+const sumTotal = document.querySelector('.sum_total-price');
+function updateSummary() {
+  sumChecked.textContent = `${checkedPrice} Won`;
+  sumUnchecked.textContent = `${uncheckedPrice} Won`;
+  sumTotal.textContent = `${checkedPrice + uncheckedPrice} Won`;
+}
+
 /*
-[작성해야할 기능]
-
-
 3. 체크된 아이템, 체크되지 않은 아이템, 총 아이템들의 가격을 업데이트하기 (아이템이 추가/삭제될 때마다)
  */
